304da95846c07ed4b436e1b88fae8ab4b0828f0a
Patrick Keistler <pkeistler@hotmail.com>
2022-09-29 23:02:59 -0400

    GPS logger now processes ubx messages

--- dashdisplay/gps_logger/gps_logger.py
+++ dashdisplay/gps_logger/gps_logger.py
@@ -2,8 +2,11 @@ from time import time
 from kivy.clock import Clock
 from functools import partial
 from serial import Serial
-from ublox_gps import UbloxGps
+from pyubx2.ubxreader import UBXReader
 import os
+from datetime import datetime
+from datetime import timedelta
+import numpy as np
 
 def monitorgps(dash):
 
@@ -11,9 +14,11 @@ def monitorgps(dash):
     init_check_period = 1.0
     main_log_start = False
 
-    port = Serial('/dev/serial0', 38400, timeout=1)
-    gps = UbloxGps(port)
+    port = Serial('/dev/ttyS0', 115200, timeout=1)
+    gps = UBXReader(port)
 
+    timezonedelta = timedelta(hours=-4)
+    starttime = None
     while True:
         if not main_log_start:
             curtime = time()
@@ -23,6 +28,25 @@ def monitorgps(dash):
             if dash.log_folder[-1] == "s":
                 continue
             main_log_start = True
-            gps_log_file = os.path.join(dash.log_folder,'gps_nmea.log')
+            gps_log_file = os.path.join(dash.log_folder,'gps_ubx.log')
+            with open(gps_log_file, 'a') as f:
+                f.write('datetime longitude latitude speed\n')
+        (raw_data, pdata) = gps.read()
         with open(gps_log_file, 'a') as f:
-            f.write(gps.stream_nmea())
+            if pdata.gnssFixOk != 1:
+                continue
+            gtime = datetime(
+                    pdata.year,
+                    pdata.month,
+                    pdata.day,
+                    pdata.hour,
+                    pdata.min,
+                    pdata.second,
+                    max(0,min(999999,int(pdata.nano*1e-3))))
+            gtime += timezonedelta
+            f.write('{} {} {} {}\n'.format(
+                #pdata.hour,pdata.min,pdata.second,max(0,min(999999,int(pdata.nano*1e-3))),
+                gtime,
+                pdata.lon,
+                pdata.lat,
+                pdata.gSpeed*0.00223694))
